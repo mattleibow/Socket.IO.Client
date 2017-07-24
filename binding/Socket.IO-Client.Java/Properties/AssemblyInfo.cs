8233fa9aff6817941913c02f50816193d2902e07
Matthew Leibowitz <mattleibow@live.com>
2017-07-24 20:04:41 +0200

    Update to v0.8.2

--- binding/Socket.IO-Client.Java/Properties/AssemblyInfo.cs
+++ binding/Socket.IO-Client.Java/Properties/AssemblyInfo.cs
@@ -27,4 +27,4 @@ using Android.App;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.8.0.0")]
-[assembly: AssemblyFileVersion("0.8.1.0")]
+[assembly: AssemblyFileVersion("0.8.2.0")]
